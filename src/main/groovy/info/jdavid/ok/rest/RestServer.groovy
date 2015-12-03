c811be24bbd160e00b6ff3e17269c007f1d7bf60
Jerome DAVID <programingjd@gmail.com>
2015-12-03 14:39:58 +0100

    -

--- src/main/groovy/info/jdavid/ok/rest/RestServer.groovy
+++ src/main/groovy/info/jdavid/ok/rest/RestServer.groovy
@@ -1,6 +1,7 @@
 package info.jdavid.ok.rest
 
 import com.squareup.okhttp.Headers
+import groovy.transform.CompileStatic
 import info.jdavid.ok.server.HttpServer
 import info.jdavid.ok.server.Response
 import info.jdavid.ok.server.StatusLines
@@ -8,12 +9,20 @@ import okio.Buffer
 
 import java.util.regex.Pattern
 
+@CompileStatic
 class RestServer extends HttpServer {
 
-  private Map<String, Map<Pattern, Closure<Response>>> handlers = [:]
+  private final Map<String, Map<Pattern, Closure<Response>>> handlers = [:]
 
-  private def register(String key, String pattern, Closure<Response> closure) {
-    (handlers[key] ?: [:].with { this.handlers[key] = it } )[~pattern] = closure
+  private RestServer register(final String key, final String pattern, final Closure<Response> closure) {
+    (handlers[key] ?: newMap(key))[~pattern] = closure
+    return this
+  }
+
+  private Map<Pattern, Closure<Response>> newMap(final String key) {
+    final Map<Pattern, Closure<Response>> map = [:]
+    handlers[key] = map
+    return map
   }
 
   RestServer clearHandlers() {
@@ -21,45 +30,54 @@ class RestServer extends HttpServer {
     return this
   }
 
-  public RestServer head(String pattern, Closure<Response> closure) {
-    register('HEAD', pattern, closure)
-    return this
+  public RestServer head(final String pattern, final Closure<Response> closure) {
+    return register('HEAD', pattern, closure)
   }
 
-  public RestServer get(String pattern, Closure<Response> closure) {
-    register('GET', pattern, closure)
-    return this
+  public RestServer get(final String pattern, final Closure<Response> closure) {
+    return register('GET', pattern, closure)
   }
 
-  public RestServer post(String pattern, Closure<Response> closure) {
-    register('POST', pattern, closure)
-    return this
+  public RestServer post(final String pattern, final Closure<Response> closure) {
+    return register('POST', pattern, closure)
   }
 
-  public RestServer put(String pattern, Closure<Response> closure) {
-    register('PUT', pattern, closure)
-    return this
+  public RestServer put(final String pattern, final Closure<Response> closure) {
+    return register('PUT', pattern, closure)
   }
 
-  public RestServer delete(String pattern, Closure<Response> closure) {
-    register('DELETE', pattern, closure)
-    return this
+  public RestServer delete(final String pattern, final Closure<Response> closure) {
+    return register('DELETE', pattern, closure)
   }
 
-  public RestServer patch(String pattern, Closure<Response> closure) {
-    register('PATCH', pattern, closure)
-    return this
+  public RestServer patch(final String pattern, final Closure<Response> closure) {
+    return register('PATCH', pattern, closure)
   }
 
   @Override
-  protected Response handle(String method, String path, Headers requestHeaders, Buffer requestBody) {
-    for (def handler in handlers[method]) {
-      def matcher = path =~ handler.key
+  protected Response handle(final String method, final String path,
+                            final Headers requestHeaders, final Buffer requestBody) {
+    def methodHandlers = handlers[method]
+    final Response response = methodHandlers == null ? null : methodHandlers.findResult { key, value ->
+      def matcher = path =~ key
       if (matcher.matches()) {
-        return handler.value(requestBody, matcher[0]?.drop(1))
+        final List<String> groups
+        if (matcher[0] instanceof List) {
+          groups = (matcher[0] as List<String>).drop(1)
+        }
+        else {
+          groups = []
+        }
+        try {
+          return value(requestBody, requestHeaders, groups)
+        }
+        catch (Exception exception) {
+          exception.printStackTrace()
+          return new Response.Builder().statusLine(StatusLines.INTERNAL_SERVER_ERROR).noBody().build()
+        }
       }
-    }
-    return new Response.Builder().statusLine(StatusLines.NOT_FOUND).noBody().build()
+    } as Response
+    return response ?: new Response.Builder().statusLine(StatusLines.NOT_FOUND).noBody().build()
   }
 
 }
--- src/test/groovy/info/jdavid/ok/rest/RestServerTest.groovy
+++ src/test/groovy/info/jdavid/ok/rest/RestServerTest.groovy
@@ -1,17 +1,20 @@
 package info.jdavid.ok.rest
 
+import com.squareup.okhttp.Headers
 import com.squareup.okhttp.HttpUrl
+import com.squareup.okhttp.MediaType
 import com.squareup.okhttp.OkHttpClient
 import com.squareup.okhttp.Protocol
 import com.squareup.okhttp.Request
 import com.squareup.okhttp.RequestBody
+import com.squareup.okhttp.ResponseBody
 import groovy.json.JsonBuilder
-import groovy.json.JsonParser
 import groovy.json.JsonSlurper
 import info.jdavid.ok.server.MediaTypes
 import info.jdavid.ok.server.Response
 import info.jdavid.ok.server.StatusLines
 import okio.Buffer
+import okio.BufferedSource
 import org.junit.AfterClass
 import org.junit.BeforeClass
 import org.junit.Test
@@ -28,9 +31,8 @@ class RestServerTest {
     new Request.Builder().url(url.build())
   }
 
-  private static OkHttpClient client() {
+  private static OkHttpClient client =
     new OkHttpClient().with { setReadTimeout(0, TimeUnit.SECONDS); it }
-  }
 
   private static RestServer server() {
     mServer.clearHandlers()
@@ -49,50 +51,153 @@ class RestServerTest {
   }
 
   @Test
-  public void test1() {
-    def testHandler = { a, b ->
-      new Response.Builder().statusLine(StatusLines.OK).noBody().build()
+  public void testHead() {
+    def testHandler = { b, h, c ->
+      new Response.Builder().statusLine(StatusLines.OK).header('test', 'ok').noBody().build()
     }
-    server().get('/test', testHandler).with {
-      def r = client().newCall(request('test').get().build()).execute()
+    server().head('/test', testHandler).with {
+      def r = client.newCall(request('test').head().build()).execute()
       assertEquals(Protocol.HTTP_1_1, r.protocol())
       assertEquals(200, r.code())
       assertEquals('0', r.header('Content-Length'))
       assertEquals('', r.body().string())
+      assertEquals('ok', r.header('test'))
+      it
+    }.with {
+      def r = client.newCall(request('test2').head().build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(404, r.code())
+      it
     }
   }
 
   @Test
-  public void test2() {
-    def testHandler = { Buffer body, List<String> it ->
-      new Response.Builder().statusLine(StatusLines.OK).body(it[0]).build()
+  public void testGet() {
+    def testHandler = { b, h, c ->
+      new Response.Builder().statusLine(StatusLines.OK).noBody().build()
+    }
+    server().get('/test', testHandler).with {
+      def r = client.newCall(request('test').get().build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(200, r.code())
+      assertEquals('0', r.header('Content-Length'))
+      assertEquals('', r.body().string())
+      it
+    }.with {
+      def r = client.newCall(request('test2').get().build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(404, r.code())
+      it
+    }
+  }
+
+  @Test
+  public void testGetParamsInUrl() {
+    def testHandler = { Buffer body, Headers headers, List<String> captures ->
+      new Response.Builder().statusLine(StatusLines.OK).body(captures[0]).build()
     }
     server().get('/test/([a-z]+)', testHandler).with {
-      def r = client().newCall(request('test', 'abc').get().build()).execute()
+      def r = client.newCall(request('test', 'abc').get().build()).execute()
       assertEquals(Protocol.HTTP_1_1, r.protocol())
       assertEquals(200, r.code())
       assertEquals('3', r.header('Content-Length'))
       assertEquals('abc', r.body().string())
+      it
+    }.with {
+      def r = client.newCall(request('test/_').get().build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(404, r.code())
+      it
     }
   }
 
   @Test
-  public void test3() {
-    def testHandler = { Buffer body, List<String> it ->
+  public void testPostParamsInUrl() {
+    def testHandler = { Buffer body, Headers headers, List<String> captures ->
       def json = new JsonSlurper().parseText(body.readUtf8())
-      json.key1 = it[0]
-      json.key2 = it[1]
+      json.key1 = captures[0]
+      json.key2 = captures[1]
       new Response.Builder().statusLine(StatusLines.OK).
         body(new JsonBuilder(json).toString(), MediaTypes.JSON).build()
     }
     server().post('/test/([a-z]+)/([0-9]+)', testHandler).with {
       def body = RequestBody.create(MediaTypes.JSON, '{"key1":null,"key2":null}')
-      def r = client().newCall(request('test', 'abc', '123').post(body).build()).execute()
+      def r = client.newCall(request('test', 'abc', '123').post(body).build()).execute()
       assertEquals(Protocol.HTTP_1_1, r.protocol())
       assertEquals(200, r.code())
       assertEquals('27', r.header('Content-Length'))
       assertTrue(r.header('Content-Type').startsWith('application/json'))
       assertEquals('{"key1":"abc","key2":"123"}', r.body().string())
+      it
+    }.with {
+      def body = RequestBody.create(MediaTypes.JSON, '')
+      def r = client.newCall(request('test', 'abc', '123').post(body).build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(500, r.code())
+      it
+    }.with {
+      def r = client.newCall(request('test', 'abc', '123').get().build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(404, r.code())
+      it
+    }
+  }
+
+  @Test
+  public void testPut() {
+    def testHandler = { Buffer body, Headers headers, List<String> captures ->
+      new Response.Builder().statusLine(StatusLines.OK).
+        body(new ResponseBody() {
+          @Override MediaType contentType() {
+            return MediaType.parse(headers.get('Content-Type'))
+          }
+          @Override long contentLength() throws IOException {
+            return Long.parseLong(headers.get('Content-Length'))
+          }
+          @Override BufferedSource source() throws IOException {
+            return body
+          }
+        }).build()
+    }
+    server().put('/test', testHandler).with {
+      String json = '{"key1":"value1"}'
+      def body = RequestBody.create(MediaTypes.JSON, json)
+      def r = client.newCall(request('test').put(body).build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(200, r.code())
+      assertEquals("${json.length()}" as String, r.header('Content-Length'))
+      assertTrue(r.header('Content-Type').startsWith('application/json'))
+      assertEquals(json, r.body().string())
+      it
+    }
+  }
+
+  @Test
+  public void testDelete() {
+    def testHandler = {  Buffer body, Headers headers, List<String> captures ->
+      new Response.Builder().statusLine(StatusLines.FORBIDDEN).noBody().build()
+    }
+    server().delete('/test', testHandler).with {
+      def r = client.newCall(request('test').delete().build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(403, r.code())
+    }
+  }
+
+  @Test
+  public void testPatch() {
+    def text = 'Forbidden'
+    def testHandler = {  Buffer body, Headers headers, List<String> captures ->
+      new Response.Builder().statusLine(StatusLines.FORBIDDEN).body(text).build()
+    }
+    server().patch('/test', testHandler).with {
+      def body = RequestBody.create(MediaTypes.TEXT, 'test')
+      def r = client.newCall(request('test').patch(body).build()).execute()
+      assertEquals(Protocol.HTTP_1_1, r.protocol())
+      assertEquals(403, r.code())
+      assertEquals("${text.length()}" as String, r.header('Content-Length'))
+      assertTrue(r.header('Content-Type').startsWith('text/plain'))
+      assertEquals(text, r.body().string())
     }
   }
 
